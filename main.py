1d6acb2e79f8130cd9590d920075aff79d419f3e
Liliya <liliyakhu@ukr.net>
2025-01-23 18:45:45 +0200

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import json
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data, separators=(",", ":")).encode("utf-8")
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    data = json.loads(json.decode("utf-8"))
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return serializer.save()
+    else:
+        raise ValueError("Invalid car data")
